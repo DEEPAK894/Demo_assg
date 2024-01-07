07585b976e08368e83e9d26dcdfe7a2579932013
DEEPAK894 <74669196+DEEPAK894@users.noreply.github.com>
2024-01-07 12:16:26 +0530

    Update my_model.sql

--- models/my_model.sql
+++ models/my_model.sql
@@ -1,7 +1,9 @@
-with raw_data as (
-  select * from orders
-)
-SELECT
-  order_id,
-  customer_id
-FROM raw_data;
+create or replace view analytics.raw.my_model as (
+    with raw_data as (
+        select * from orders
+    )
+    select
+        order_id,
+        customer_id
+    from raw_data
+);
